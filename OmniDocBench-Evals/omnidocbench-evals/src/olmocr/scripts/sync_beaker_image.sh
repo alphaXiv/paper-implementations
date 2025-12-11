d3dc009cb9cf1ff432df84a3e4f100e8921e7558
YuvrajSingh-mist <yuvraj.mist@gmail.com>
2025-12-11 20:32:55 +0530

    Restrcutured the folders of omnidocbench and agent-r1

--- OmniDocBench-Evals/omnidocbench-evals/src/olmocr/scripts/sync_beaker_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-VERSION=$(python -c 'import olmocr.version; print(olmocr.version.VERSION)')
-echo "$VERSION"
-
-docker pull alleninstituteforai/olmocr:v$VERSION
-beaker image create --workspace ai2/oe-data-pdf --name olmocr-inference-$VERSION alleninstituteforai/olmocr:v$VERSION
