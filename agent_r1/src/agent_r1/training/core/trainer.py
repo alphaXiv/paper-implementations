c4f923adc44fa4381d8c46732a4ea22d9ad52007
Rehaan Ahmad <rehaanahmad03@gmail.com>
2025-12-24 10:09:40 +0000

    remove console

--- agent_r1/src/agent_r1/training/core/trainer.py
+++ agent_r1/src/agent_r1/training/core/trainer.py
@@ -387,7 +387,6 @@ class RayAgentTrainer(object):
                         reward_extra_infos_dict: dict[str, list]
                         batch.batch["token_level_scores"] = reward_tensor
 
-                        print(f"{list(reward_extra_infos_dict.keys())=}")
                         if reward_extra_infos_dict:
                             batch.non_tensor_batch.update({k: np.array(v) for k, v in reward_extra_infos_dict.items()})
 
