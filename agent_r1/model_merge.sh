29035549ed56fd4af709dd8843e582a891611620
Yuvraj Singh <yuvraj.mist@gmail.com>
2025-12-26 04:19:23 +0530

    Delete agent_r1/model_merge.sh

--- agent_r1/model_merge.sh
+++
@@ -1,5 +0,0 @@
-export CHECKPOINT_DIR=checkpoints/hotpotqa/ppo-qwen2.5-1.5b-instruct/global_step_1/actor
-export HF_MODEL_PATH=Qwen/Qwen2.5-1.5B-Instruct
-export TARGET_DIR=./converted_model
-
-python3 verl/scripts/model_merger.py --backend fsdp --hf_model_path $HF_MODEL_PATH --local_dir $CHECKPOINT_DIR --target_dir $TARGET_DIR
